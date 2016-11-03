d6b24594a8c9b5cbb0d50af6918e2a7a7b77a8f4
zhou <1458523906@qq.com>
2016-11-03 18:59:01 +0800

    主页面布局编写

--- ChatDemoUI3.0/src/com/zhou/superwechat/SuperWeChatHelper.java
+++ ChatDemoUI3.0/src/com/zhou/superwechat/SuperWeChatHelper.java
@@ -1315,7 +1315,7 @@ public class SuperWeChatHelper {
      * @return
      */
     public Map<String, User> getAppContactList() {
-        if (isLoggedIn() && appContactList == null) {
+        if (isLoggedIn() && (appContactList == null || appContactList.size() ==0)) {
             appContactList = demoModel.getAppContactList();
         }
 
