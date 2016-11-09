62674c0b35af22cd259b8def36c51e205ce9fe83
zhou <1458523906@qq.com>
2016-11-09 09:07:54 +0800

    删除“添加到黑名单”选项

--- ChatDemoUI3.0/src/com/zhou/superwechat/ui/ContactListFragment.java
+++ ChatDemoUI3.0/src/com/zhou/superwechat/ui/ContactListFragment.java
@@ -227,10 +227,11 @@ public class ContactListFragment extends EaseContactListFragment {
                 e.printStackTrace();
             }
             return true;
-        } else if (item.getItemId() == R.id.add_to_blacklist) {
+        } /* // 去除黑名单功能
+        else if (item.getItemId() == R.id.add_to_blacklist) {
             moveToBlacklist(toBeProcessUsername);
             return true;
-        }
+        }*/
         return super.onContextItemSelected(item);
     }
 
