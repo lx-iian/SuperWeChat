5dc4ee256ebafd520d611d6672434cbdf7b8953f
zhou <1458523906@qq.com>
2016-11-10 11:27:21 +0800

    好友资料的视频通话

--- ChatDemoUI3.0/src/com/zhou/superwechat/ui/FriendProfileActivity.java
+++ ChatDemoUI3.0/src/com/zhou/superwechat/ui/FriendProfileActivity.java
@@ -6,7 +6,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.hyphenate.chat.EMClient;
 import com.hyphenate.easeui.domain.User;
 import com.hyphenate.easeui.utils.EaseUserUtils;
 import com.zhou.superwechat.I;
@@ -90,6 +92,14 @@ public class FriendProfileActivity extends BaseActivity {
                 MFGT.gotoChat(this, user.getMUserName());
                 break;
             case R.id.btn_add_send_video:
+                if (!EMClient.getInstance().isConnected())
+                    Toast.makeText(this, R.string.not_connect_to_server, Toast.LENGTH_SHORT).show();
+                else {
+                    startActivity(new Intent(this, VideoCallActivity.class).putExtra("username", user.getMUserName())
+                            .putExtra("isComingCall", false));
+                    // videoCallBtn.setEnabled(false);
+                    // inputMenu.hideExtendMenuContainer();
+                }
                 break;
         }
     }
